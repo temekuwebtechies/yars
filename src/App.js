d8b914de4f265d4b5c9cf6d7a4d4a545117b7213
Eyasu Kifle <kpsychwave@gmail.com>
2017-08-05 01:56:34 +0000

    Added basic routes / components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Account from './components/Account.js';
+import Home from './components/Home.js';
+import Login from './components/Login.js';
+import Recover from './components/Recover.js';
+import SignUp from './components/SignUp.js';
+
+const Nav = () => (
+    <ul>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/signup">Sign Up</Link></li>
+      <li><Link to="/login">Login</Link></li>
+      <li><Link to="/recover">Recover</Link></li>
+      <li><Link to="/account">My Account</Link></li>
+    </ul>
+)
 
 class App extends Component {
+  
+  initRoutes() {
+    return (
+    <Router>
+    <div>
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/recover" component={Recover} />
+        <Route exact path="/account" component={Account} />
+      </Switch>
+    </div>
+    </Router>
+    )
+  }
+  
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main>
+        {this.initRoutes()}
+      </main>
     );
   }
 }
