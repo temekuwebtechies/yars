9aa6d30101585e2f2d68e76cabf1c365b4433bd4
Eyasu Kifle <kpsychwave@gmail.com>
2017-08-05 02:34:27 +0000

    Populated login component, mock API, message service

--- src/App.js
+++ src/App.js
@@ -21,27 +21,83 @@ const Nav = () => (
 
 class App extends Component {
   
-  initRoutes() {
-    return (
-    <Router>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={SignUp} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/recover" component={Recover} />
-        <Route exact path="/account" component={Account} />
-      </Switch>
-    </div>
-    </Router>
-    )
+  constructor(props) {
+    super(props);
+    
+    this.state = {
+      isLoggedIn: false,
+      name: "Anonymous",
+      message: {
+        type: "hidden",
+        text: ""
+      }
+    }
+    
+    
+    this.showMessage = this.showMessage.bind(this);
+    this.login = this.login.bind(this);
+    this.register = this.register.bind(this);
+  }
+  
+  showMessage (type = "hidden", text = "") {
+    this.setState({message: {type, text}});
+  }
+  
+  apiCall() {
+    console.info("Calling API");
+    return new Promise((resolve, reject) => {
+      console.info("Got API Result");
+      setTimeout(resolve, Math.random() * 3000);
+    });
+  }
+  
+  // Login to auth service
+  login (email, password) {
+    let { isLoggedIn, name } = this.state;
+    
+    this.apiCall().then(() => {
+      if (Math.random(0) > 0.5) {
+        isLoggedIn = true;
+        name = "Authenticated";
+        this.showMessage('success', 'Login successful');
+      } else {
+        isLoggedIn = false;
+        name = "Stranger";
+        this.showMessage('error', 'Login failed!');
+      }
+      this.setState({isLoggedIn, name});
+    });
+  }
+  
+  register (email, password) {
+    // API Calls
   }
   
   render() {
+    
+    const { name, message } = this.state;
+    const messageStyles = {
+      color: message.type === 'error' ? 'red' : 'green'
+    }
+    
     return (
       <main>
-        {this.initRoutes()}
+        <Router>
+        <div>
+          <Nav />
+          <h1>Welcome {name}</h1>
+          <p onClick={() => this.showMessage()} hidden={message.type === "hidden"} style={messageStyles}>{message.text}</p>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/signup" component={SignUp} />
+            <Route exact path="/login">
+              <Login login={this.login} />
+            </Route>
+            <Route exact path="/recover" component={Recover} />
+            <Route exact path="/account" component={Account} />
+          </Switch>
+        </div>
+        </Router>
       </main>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,3 +1,8 @@
 import React, { Component } from 'react';
 
-export default () => <div>Login</div>
+export default (props) => {
+console.log("Login props", props);
+return (<div>Login
+    <button onClick={() => {props.login("test", "test")}}>Login</button>
+</div>)
+}
